2b6a1e5bb0bd3fdd2d9c15c7839aac734541431d
kryla-angela <krylaangel@gmail.com>
2025-08-15 23:27:29 +0300

    theme and auth

--- data/users.js
+++ data/users.js
@@ -3,19 +3,16 @@ const users = [
     id: 1,
     name: "John",
     age: 28,
-    password: 123,
   },
   {
     id: 2,
     name: "Johan",
     age: 18,
-    password: 123,
   },
   {
     id: 3,
     name: "Mary",
     age: 38,
-    password: 123,
   },
 ];
 module.exports = { users };
